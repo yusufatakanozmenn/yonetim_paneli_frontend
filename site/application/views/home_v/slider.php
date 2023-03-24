767a8a20ba0c80db7db3d66f0a2f6117f0f3135d
Onur Doğu Altınışık <94872156+onrdg@users.noreply.github.com>
2023-03-24 16:10:04 +0300

    slider

--- site/application/views/home_v/slider.php
+++ site/application/views/home_v/slider.php
@@ -6,9 +6,9 @@ $slides = $this->slider_model->get_all(array("durum" => 1), "rank ASC");
 
 <div id="masterslider" class="master-slider ms-skin-default">
     <?php foreach ($slides as $slider) { ?>
-        <div class="ms-slide" data-fill-mode="fill">
+        <div class="ms-slide " data-fill-mode="fill">
             <img src="./masterslider/style/blank.gif" data-src="<?= base_url() . 'panel/uploads/slider_v/'.$slider->resim ?>" alt="">
-            <h2 class="ms-layer pi-caption01" style="top: 410px; left: 450px;" data-type="text" data-effect="bottom(40)" data-duration="1000" data-ease="easeOutExpo" data-delay="800">
+            <h2 class="ms-layer pi-caption01" style="top: 410px; left: 730px;" data-type="text" data-effect="bottom(40)" data-duration="1000" data-ease="easeOutExpo" data-delay="800">
                 <?= $slider->adi ?>
             </h2>
             <div class="ms-layer divider" style="top: 505px; left: 737px;" data-type="text" data-effect="top(40)" data-duration="1000" data-ease="easeOutExpo" data-delay="1000">
